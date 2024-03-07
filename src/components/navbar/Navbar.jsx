318568ea71269b6acfaa00fb50c07129bac0da16
Prathamesh <prathamesh@technosive.co.uk>
2024-03-07 16:14:55 +0530

    feat: Minor changes in UI

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,91 +1,107 @@
-import { Link, useNavigate } from "react-router-dom";
-import SearchBar from "../searchBar/SearchBar";
+import { ShoppingCartIcon } from "lucide-react";
 import { useSelector } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+// import SearchBar from "../searchBar/SearchBar";
 
+// New Navbar
+// https://www.material-tailwind.com/docs/react/navbar
 
 const Navbar = () => {
-    // get user from localStorage 
-    const user = JSON.parse(localStorage.getItem('users'));
+  // get user from localStorage
+  const user = JSON.parse(localStorage.getItem("users"));
 
-    // navigate 
-    const navigate = useNavigate();
+  // navigate
+  const navigate = useNavigate();
 
-    // logout function 
-    const logout = () => {
-        localStorage.clear('users');
-        navigate("/login")
-    }
+  // logout function
+  const logout = () => {
+    localStorage.clear("users");
+    navigate("/login");
+  };
 
-    // CartItems
-    const cartItems = useSelector((state) => state.cart);
+  // CartItems
+  const cartItems = useSelector((state) => state.cart);
 
-    // navList Data
-    const navList = (
-        <ul className="flex space-x-3 text-white font-medium text-md px-5 ">
-            {/* Home */}
-            <li>
-                <Link to={'/'}>Home</Link>
-            </li>
+  // navList Data
+  const navList = (
+    <ul className="flex space-x-3 text-white font-medium text-md px-5 ">
+      {/* Home */}
+      {/* <li className="hover:underline">
+        <Link to={"/"}>Home</Link>
+      </li> */}
 
-            {/* All Product */}
-            <li>
-                <Link to={'/allproduct'}>All Product</Link>
-            </li>
+      {/* All Product */}
+      <li className="hover:underline">
+        <Link to={"/allproduct"}>All Products</Link>
+      </li>
 
-            {/* Signup */}
-            {!user ? <li>
-                <Link to={'/signup'}>Signup</Link>
-            </li> : ""}
+      {/* Cart */}
+      <li className="">
+        <Link to={"/cart"}>
+          <p className="flex flex-row relative">
+            <ShoppingCartIcon className="size-4" />
+            <sup className="text-xs">{cartItems.length}</sup>
+          </p>
+        </Link>
+      </li>
 
-            {/* Signup */}
-            {!user ? <li>
-                <Link to={'/login'}>Login</Link>
-            </li> : ""}
+      {/* Signup */}
+      {!user ? (
+        <li className="hover:underline">
+          <Link to={"/login"}>Login</Link>
+        </li>
+      ) : null}
 
-            {/* User */}
-            {user?.role === "user" && <li>
-                <Link to={'/user-dashboard'}>User</Link>
-            </li>}
+      {/* Signup */}
+      {!user ? (
+        <li className="hover:underline">
+          <Link to={"/signup"}>Signup</Link>
+        </li>
+      ) : null}
 
-            {/* Admin */}
-            {user?.role === "admin" && <li>
-                <Link to={'/admin-dashboard'}>Admin</Link>
-            </li>}
+      {/* User */}
+      {user?.role === "user" && (
+        <li className="hover:underline">
+          <Link to={"/user-dashboard"}>User</Link>
+        </li>
+      )}
 
-            {/* logout */}
-            {user && <li className=" cursor-pointer" onClick={logout}>
-                logout
-            </li>}
+      {/* Admin */}
+      {user?.role === "admin" && (
+        <li className="hover:underline">
+          <Link to={"/admin-dashboard"}>Admin</Link>
+        </li>
+      )}
 
-            {/* Cart */}
-            <li>
-                <Link to={'/cart'}>
-                    Cart({cartItems.length})
-                </Link>
-            </li>
-        </ul>
-    )
-    return (
-        <nav className="bg-pink-600 sticky top-0">
-            {/* main  */}
-            <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
-                {/* left  */}
-                <div className="left py-3 lg:py-0">
-                    <Link to={'/'}>
-                        <h2 className=" font-bold text-white text-2xl text-center">E-Bharat</h2>
-                    </Link>
-                </div>
+      {/* logout */}
+      {user && (
+        <li className="hover:underline cursor-pointer" onClick={logout}>
+          Logout
+        </li>
+      )}
+    </ul>
+  );
+  return (
+    <nav className="bg-pink-600 sticky top-0">
+      {/* main  */}
+      <div className="lg:flex lg:justify-between items-center py-3 lg:px-3 ">
+        {/* left  */}
+        <div className="left py-3 lg:py-0">
+          <Link to={"/"}>
+            <h2 className=" font-bold text-white text-2xl text-center">
+              E-Bharat
+            </h2>
+          </Link>
+        </div>
 
-                {/* right  */}
-                <div className="right flex justify-center mb-4 lg:mb-0">
-                    {navList}
-                </div>
+        {/* right  */}
+        <div className="right flex justify-center mb-4 lg:mb-0">{navList}</div>
 
-                {/* Search Bar  */}
-                <SearchBar />
-            </div>
-        </nav>
-    );
-}
+        {/* Search Bar  */}
+        {/* <SearchBar /> */}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
